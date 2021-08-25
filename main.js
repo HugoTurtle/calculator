4a5492d8000edc14a98198638156e3ee3e925e0b
Hugo Perez <hugopereztm12309@gmail.com>
2021-08-25 12:46:29 -0400

    Created basic math functions, and an operate function which will take in two numbers and then call one of our math functions and return that total.

---
+++ main.js
@@ -0,0 +1,18 @@
+//Basic Math Functions//
+function add(num1, num2) {
+    return num1 + num2;
+}
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+function divide(num1, num2) {
+    return num1 / num2;
+}
+//Operate Function//
+function operate(num1, num2) {
+    return add(num1,num2);
+}
+console.log(operate(5,4));
